95609b3dc973c71d19413199d2fcb6c3195a2f03
benjamin <balberts023@student.wethinkcode.co.za>
2024-07-10 21:08:07 +0200

    Basic functionality of Admin Dashbard Page is done.

--- ote-ecom/src/app/admin/page.tsx
+++ ote-ecom/src/app/admin/page.tsx
@@ -1,12 +1,15 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import db from "@/db/db"
 import { formatNumber, formatCurrency } from "@/lib/formatters";
+import { resolve } from "path";
 
-async function getSalesData(){
+async function getSalesData() {
     const data = await db.order.aggregate({
-        _sum: {pricePaidInCents: true},
+        _sum: { pricePaidInCents: true },
         _count: true
     })
+
+    await wait(2000)
 
     return {
         amount: (data._sum.pricePaidInCents || 0) / 100,
@@ -14,16 +17,45 @@ async function getSalesData(){
     }
 }
 
+function wait(duration: number) {
+    return new Promise(resolve => setTimeout(resolve, duration))
+}
+
+async function getUserData() {
+    const [userCount, orderData] = await Promise.all([
+        db.user.count(),
+        db.order.aggregate({ _sum: { pricePaidInCents: true }, }),])
+
+    return {
+        userCount,
+        averageValuePerUser: userCount === 0 ? 0 : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
+    }
+}
+
+async function getProductData() {
+    const [activeCount, inactiveCount] = await Promise.all([
+    db.product.count({where: {isAvailableForPurchase: true}}),
+    db.product.count({where: {isAvailableForPurchase: false}})
+    ])
+
+    return {activeCount, inactiveCount}
+}
+
 export default async function AdminDashboard() {
-    const salesData = await getSalesData()
+    const [salesData, userData, productData] = await Promise.all([
+        getSalesData(),
+        getUserData(),
+        getProductData()
+    ])
+
     // return <h1>Dashboard</h1>
     return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <DashboardCard title = "Sales" subtitle={formatNumber(salesData.numberOfSales)} body={formatCurrency(salesData.amount)} />
-        <DashboardCard title = "Sales" subtitle="test" body="body" />
-        <DashboardCard title = "Sales" subtitle="test" body="body" />
-        <DashboardCard title = "Sales" subtitle="test" body="body" />
+        <DashboardCard title="Sales" subtitle={`${formatNumber(salesData.numberOfSales)} Orders`} body={formatCurrency(salesData.amount)} />
+        <DashboardCard title="Customer" subtitle={`${formatCurrency(userData.averageValuePerUser)} Average Value`} body={formatNumber(userData.userCount)} />
+        <DashboardCard title="Active Products" subtitle={`${formatNumber(productData.inactiveCount)} Inactive`} body={formatNumber(productData.activeCount)} />
     </div>
 }
+
 
 type DashBoardCardProps = {
     title: string
